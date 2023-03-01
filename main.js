5644c7ef7b3bda295318831159c5ed52b19b2610
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-03-01 17:00:53 +0300

    add numbers

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function clock (){
     const hoursArrow = document.querySelector('.hours');
     const minutesArrow = document.querySelector('.minutes');
     const secondsArrow = document.querySelector('.seconds');
+    const numberHours = document.querySelector('.number__hours');
+    const numberMinutes = document.querySelector('.number__minutes');
+    const numberSeconds= document.querySelector('.number__seconds');
+
     const deg = 6;
 
     setInterval(() => {
@@ -14,7 +18,19 @@ function clock (){
         hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
         minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
         secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+
+        numberHours.innerHTML = getZero(day.getHours());
+        numberMinutes.innerHTML = getZero(day.getMinutes());
+        numberSeconds.innerHTML = getZero(day.getSeconds());
+
     }, 0)
 }
+function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
 
 clock();
